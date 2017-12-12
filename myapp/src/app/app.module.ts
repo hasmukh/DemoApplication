122f7625d1038c77e7e7d8f7edbf041fa38777aa
Maniya Hasmukh Jivanbhai (RBEI/BSJ3) <hmi5kor@bosch.com>
2017-12-12 08:15:44 +0530

    Integrated dragula for drag and drop

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DragulaModule } from 'ng2-dragula';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,12 +9,13 @@ import { BoxesComponent } from './boxes/boxes.component';
 import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
 import { MyDraggableDirective } from './shared/draggable.drirective';
 import { AngularDraggableModule} from 'angular2-draggable';
+import { DragulaComponent } from './dragula/dragula.component';
 
 // Router configurations
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'boxes', component: BoxesComponent },
-  {path: 'other', component: PageNotFoundComponent},
+  {path: 'dragula', component: DragulaComponent},
   { path: '',   redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
@@ -24,12 +26,14 @@ const appRoutes: Routes = [
     HomeComponent,
     BoxesComponent,
     PageNotFoundComponent,
-    MyDraggableDirective
+    MyDraggableDirective,
+    DragulaComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
-    AngularDraggableModule
+    AngularDraggableModule,
+    DragulaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
